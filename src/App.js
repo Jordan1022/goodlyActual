fae405f3e6c14ae50afb6c3edae7c1e7765837b6
goodly admin <admin@goodlydevelopment.com>
2020-10-04 18:27:48 -0700

    adjusted names, put together some of contact page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import goodlyLogo from './goodlyLogo.svg';
 import './App.css';
 import './LoginForm.js';
 import {Switch, Route} from 'react-router-dom'
-import landingPage from './pages/Landing Page/landingPage';
+import landingPage from './pages/LandingPage/landingPage';
+import contact from './pages/ContactPage/contactPage'
 
 const App = () => {
   
@@ -11,6 +12,7 @@ const App = () => {
     <div className="App">
       <Switch>
         <Route exact path="/" component={landingPage}/>
+        <Route exact path="contact" component={contact}/>
       </Switch>
     </div>
   );
---
+++ src/pages/ContactPage/contactPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import './styles.css'
+
+const contact = () => {
+    return(
+        <Grid>
+            
+        </Grid>
+    )
+}
+
+export default contact;
