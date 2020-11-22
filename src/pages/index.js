f9a60af1be07fca11db6b029e44992c80cda4dc6
goodly admin <admin@goodlydevelopment.com>
2020-11-22 15:09:00 -0800

    meta added

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Goodly Development'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Goodly Development'} />
       </Helmet>
       <App />
     </>
