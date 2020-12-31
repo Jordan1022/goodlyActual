ef8d19951d44924cdd7c2fd543b17f1e9b02f0a5
jordan-veracitek <jordana@veracitek.com>
2020-12-31 11:50:38 -0800

    changed archaic to old on subtitle of data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: 'Welcome to',
   name: 'Goodly Development',
-  subtitle: 'Goodly: (Archaic English) of good quality, abundant.',
+  subtitle: 'Goodly: (Old English) of good quality, abundant.',
   cta: 'Know more',
 };
 
