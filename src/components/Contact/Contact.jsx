d001690f5ed93055c5d6979d8f8f0cc1a8c104e6
jordan-veracitek <land.of.jordan@gmail.com>
2021-06-28 15:56:02 -0700

    wording change and npm updates

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with us? Awesome!'}
+              {cta || 'Looking for developers?'}
             </p>
             <a
               target="_blank"
