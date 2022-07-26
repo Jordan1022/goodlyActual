9549b39456248c96090d2d187fd5eeffcdc1d4b2
Jordan_A <land.of.jordan@gmail.com>
2022-07-26 09:57:28 -0500

    stripped down to maintenance page

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Coming Soon
+        </h2>
+        <p
+        >
+          A new improved site
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
